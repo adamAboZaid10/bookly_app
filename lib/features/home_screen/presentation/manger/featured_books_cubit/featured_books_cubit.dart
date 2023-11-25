c7309a0925b7c965c07390f0b0a388bc9399d3a6
adam <adamemail@gmail.com>
2023-11-25 10:53:48 +0200

    featured books meathod

--- lib/features/home_screen/presentation/manger/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home_screen/presentation/manger/featured_books_cubit/featured_books_cubit.dart
@@ -1,9 +1,24 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly_app/core/errors/failures.dart';
 import 'package:bookly_app/features/home_screen/data/model/book/book.model.dart';
+import 'package:bookly_app/features/home_screen/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+  HomeRepo homeRepo;
+
+  Future<void> featchFeaturedBooks ()async
+  {
+    emit(FeaturedBooksLoading());
+   var result = await homeRepo.fetchFeaturedBooks();
+
+   result.fold((failure) {
+    emit(FeaturedBooksError(failure.errorMessage));
+   }, (books) {
+    emit(FeaturedBooksSuccess(books));
+   });
+  } 
 }
